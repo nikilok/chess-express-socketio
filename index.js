d4b99eda682bf5af4860780077eb4028c7a16cc3
Nikil K <nikilok@gmail.com>
2019-05-05 12:03:26 +0200

    Using CORS pacakge for cors support

--- index.js
+++ index.js
@@ -5,15 +5,8 @@ const http = require("http").Server(app, {
 });
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3001;
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+const cors = require("cors");
+app.use(cors);
 /**
  * OnGoing games Object
  */
